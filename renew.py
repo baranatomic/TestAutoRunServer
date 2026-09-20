d127cdf7175072aa22e9ad1565ddc7f05974cc23
baranatomic <brbco90@atomicmail.io>
2026-09-20 15:24:33 +0330

    Update renew.py

--- renew.py
+++ renew.py
@@ -4,7 +4,7 @@ from playwright.sync_api import sync_playwright
 
 EMAIL = os.environ.get("KATABUMP_EMAIL")
 PASSWORD = os.environ.get("KATABUMP_PASSWORD")
-SERVER_ID = os.environ.get("SERVER_ID", "d1712508")
+SERVER_ID = os.environ.get("SERVER_ID")
 
 def main():
     if not EMAIL or not PASSWORD:
